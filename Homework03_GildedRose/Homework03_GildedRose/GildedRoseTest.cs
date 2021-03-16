3da979ce8de39b69ff03120a768108b2258b4aad
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-03-16 22:15:31 +0200

    get_problem_git

---
+++ Homework03_GildedRose/Homework03_GildedRose/GildedRoseTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace Homework03_GildedRose
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equals("fixme", Items[0].Name);
+        }
+    }
+}
---
+++ Homework03_GildedRose/Homework03_GildedRose/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework03_GildedRose
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+    }
+}
